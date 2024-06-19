7a5aef3731c61b7ccc5aadb9e8e9cc94ebce4ce4
Riley Stonehouse <riley.stonehouse@gmail.com>
2024-06-19 04:49:49 +0000

    Complete Task C of Challenge. Fix N+1 issues with preloading.

--- earnings classifier (backend, hard)/app/models/earnings_classifier.rb
+++ earnings classifier (backend, hard)/app/models/earnings_classifier.rb
@@ -7,6 +7,10 @@ class EarningsClassifier
 
   def initialize(timesheet)
     @timesheet = timesheet
+    ActiveRecord::Associations::Preloader.new(
+      records: [@timesheet],
+      associations: { shifts: :earnings }
+    ).call
   end
 
   ##
@@ -28,6 +32,7 @@ class EarningsClassifier
 
   def process_hours_rules(shift, earnings_context)
     hours_rules = HoursRule.ordered_by_rank
+    preload_rules_associations(hours_rules)
     hours_rules.each do |rule|
       break unless earnings_context.remaining_hours?
 
@@ -41,8 +46,27 @@ class EarningsClassifier
 
   def process_addon_rules(shift, earnings_context)
     addon_rules = EarningRule.where.not(type: 'HoursRule')
+    preload_rules_associations(addon_rules)
     addon_rules.each do |rule|
       shift.earnings.build(earning_rule: rule, units: rule.units) if rule.addon?(earnings_context)
     end
+  end
+
+  def preload_rules_associations(rules)
+    ActiveRecord::Associations::Preloader.new(
+      records: rules,
+      associations: :earning_tests
+    ).call
+
+    earnings_tests = rules.flat_map(&:earning_tests)
+    ActiveRecord::Associations::Preloader.new(
+      records: earnings_tests.select { |et| et.is_a?(MaximumHoursTest) },
+      associations: :maximum_hours_test_config
+    ).call
+
+    ActiveRecord::Associations::Preloader.new(
+      records: earnings_tests.select { |et| et.is_a?(DayOfWeekTest) },
+      associations: :day_of_week_test_config
+    ).call
   end
 end
